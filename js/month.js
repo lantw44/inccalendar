a9a008bd920424583e29103437c9f660ca3ad8cf
藍挺瑋 <lantw44@gmail.com>
2012-11-27 02:12:59 +0800

    月曆版面初稿已完成

--- js/month.js
+++ js/month.js
@@ -11,19 +11,19 @@ function setyearmonth(){
 	/* 初始化 objyear 和 objmonth，其他函式中不用在做同樣的動作 */
 	objyear = document.getElementById("timeselect_year");
 	objmonth = document.getElementById("timeselect_month");
-	if(useryear > 0 && usermonth >= 1 && usermonth <= 12){
+	if(useryear >= 1970 && usermonth >= 1 && usermonth <= 12){
 		value_year = useryear;
 		value_month = usermonth;
 	}else{
 		var today = new Date();
 		value_year = today.getFullYear();
-		value_month = today.getMonth();	
+		value_month = today.getMonth() + 1;	
 	} 
 }
 
 function timeselect_prev(){
 	if(value_month <= 1){
-		if(value_year > 1){
+		if(value_year > 1970){
 			value_year--;
 			value_month = 12;
 		}
@@ -60,8 +60,14 @@ function timeselect_direct(){
 function timeedit_apply(){
 	var newyear = parseInt(document.getElementById("timeedit_year").value);
 	var newmonth = parseInt(document.getElementById("timeedit_month").value);
-	if(newyear <= 0 || newmonth < 1 || newmonth > 12){
-		alert("請輸入正確的年份和月份！");
+	if(!isFinite(newyear) || !isFinite(newmonth)){
+		alert("請輸入正確的數字！");
+		return;
+	}else if(newyear < 1970){
+		alert("請輸入 1970 年以後的年份！");
+		return;
+	}else if(newmonth < 1 || newmonth > 12){
+		alert("請輸入正確的月份！");
 		return;
 	}
 	value_year = newyear;
@@ -83,7 +89,19 @@ function timeedit_cancel(){
 
 function createcaltable(){
 	objbd = document.getElementById("calbody");
-	
+	var i, j;
+	var node_tr;
+	var node_td;
+	for(i=1; i<=5; i++){
+		node_tr = document.createElement("tr");
+		node_tr.setAttribute("id", "calrow" + i.toString());
+		for(j=0; j<7; j++){
+			node_td = document.createElement("td");
+			node_td.setAttribute("id", "cal" + i.toString() + j.toString());
+			node_tr.appendChild(node_td);
+		}
+		objbd.appendChild(node_tr);
+	}
 }
 
 function setmonthcal(){
@@ -91,5 +109,45 @@ function setmonthcal(){
 	objmonth.innerHTML = value_month;
 	setcookievalue(cookie_year, value_year);
 	setcookievalue(cookie_month, value_month);
+	var toyear = value_year;
+	var tomonth = value_month - 1;
+	var todate = 1;
+	var od_day;
+	var od_month;
+	var od_year;
+	var objdate = new Date();
+	var objcal;
+	var i;
+	objdate.setFullYear(toyear, tomonth, todate);
+	while((od_day = objdate.getDay()) != 0){
+		todate = objdate.getDate();
+		objdate.setDate(--todate);
+	}
+	for(i=1 ;i<=5 ;objdate.setDate(++todate)){
+		todate = objdate.getDate();
+		od_day = objdate.getDay();
+		od_month = objdate.getMonth();
+		od_year = objdate.getYear();
+		objcal = document.getElementById("cal" + i.toString() + od_day.toString());
+		if(od_month == tomonth){
+			objcal.style.borderColor = "black";
+			objcal.style.color = "black";
+			objcal.innerHTML = todate;
+		}else if(od_year < toyear || od_month < tomonth){
+			objcal.style.borderColor = "lightgray";
+			objcal.style.color = "lightgray";
+			objcal.innerHTML = (od_month + 1).toString() + "/" + todate.toString();
+		}else if(od_year > toyear || od_month > tomonth){
+			objcal.style.borderColor = "lightgray";
+			objcal.style.color = "lightgray";
+			objcal.innerHTML = (od_month + 1).toString() + "/" + todate.toString();
+		}else{
+			alert("setmonthcal(): fatal error");
+			return;
+		}
+		if(od_day >= 6){
+			i++;
+		}
+	}
 }
 
