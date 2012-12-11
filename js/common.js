72fa2b3f3fae44772343f6b1cc78e46ab3b21538
藍挺瑋 <lantw44@gmail.com>
2012-12-12 02:33:40 +0800

    狀態列現在可以設定為警告訊息（紅色），並且提供 stack 來儲存和取回訊息。

--- js/common.js
+++ js/common.js
@@ -3,19 +3,60 @@ var stbdic = {
 	"loading": "載入中......",
 	"completed": "完成"
 };
+var stbstack;
+var stbstr = "";
+var stbwarned = 0;
 
 function status_bar_init(){
 	stbobj = document.getElementById("statusbar");
+	stbstack = new Array();
 }
 
 function status_bar_set(msgstr){
+	stbstr = msgstr;
+	stbwarned = false;
+	stbobj.style.color = "white";
 	stbobj.innerHTML = msgstr;
+}
+
+function status_bar_warning(msgstr){
+	stbstr = msgstr;
+	stbwarned = true;
+	stbobj.style.color = "red";
+	stbobj.innerHTML = msgstr;
+}
+
+function status_bar_append(msgstr){
+	stbstr = stbstr + " " + msgstr;
+	stbobj.innerHTML = stbstr;
+}
+
+function status_bar_clear(){
+	stbstr = ""
+	stbobj.innerHTML = stbstr;
+}
+
+function status_bar_save(){
+	var stbnewnode = new Object();
+	stbnewnode.str = stbstr;
+	stbnewnode.warn = stbwarned;
+	stbstack.push(stbnewnode);
+}
+
+function status_bar_restore(){
+	var stbgetnode = stbstack.pop();
+	if(stbgetnode.warn){
+		status_bar_warning(stbgetnode.str);
+	}else{
+		status_bar_set(stbgetnode.str);
+	}
 }
 
 function status_bar_special(mgstr){
 	for(spstr in stbdic){
 		if(mgstr == spstr){
-			stbobj.innerHTML = stbdic[spstr];
+			stbstr = stbdic[spstr];
+			stbobj.innerHTML = stbstr;
 		}
 	}
 }
