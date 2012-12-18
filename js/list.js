3fc4377de9d789049dc2c0ecd38be089946a1072
黃勁捷 <pizza159630@gmail.com>
2012-12-18 22:43:48 +0800

    修改了搜尋活動的問題 增加內容搜尋選項

--- js/list.js
+++ js/list.js
@@ -3,18 +3,18 @@
 	var dataclass = ["date", "week", "time", "title"];
 	var eventid, dataid;
 	// for (var month = 1 ; month <= 12 ; month++) {
-		// calevent = inccal_fetch (year, month);
+		// calevent = inccal_fetch (2012, month);
 		for (var i = 0 ; i < 10 ; i++) {
 			eventid = "event" + (i + 1);
-			$ ("#eventbody").append ("<tr id = \"" + eventid + "\"></tr>");
-			$ ("#" + eventid).addClass ("event");
+			$ ("#eventbody").append ("<tr id = \"" + eventid + "head\"></tr>");
+			$ ("#" + eventid + "head").addClass ("event");
 			for (var j = 0 ; j < 4 ; j++) {
 				dataid = eventid + dataclass[j];
-				$ ("#" + eventid).append ("<td id = \"" + dataid + "\"></td>");
+				$ ("#" + eventid + "head").append ("<td id = \"" + dataid + "\"></td>");
 				$ ("#" + dataid).addClass (dataclass[j]);
 				$ ("#" + dataid).attr ({"onclick":"togglecontent (this.id);"});
 			}
-			$ ("#eventbody").append ("<tr class = \"event\"><td id = \"" + eventid + "content\"></td></tr>");
+			$ ("#eventbody").append ("<tr id = \"" + eventid + "body\" class = \"event\"><td id = \"" + eventid + "content\"></td></tr>");
 			$ ("#" + eventid + "content").addClass ("content");
 			$ ("#" + eventid + "content").attr ({"colspan":"4"});
 			$ ("#" + eventid + "content").css ("display", "none");
@@ -23,36 +23,54 @@
 }
 
 function searchevent () {//搜尋符合的活動
-	var obj = document.getElementById ("searchingoption");//obj = <select>
-	var option = obj[obj.selectedIndex].value;//option = date or content
-	var events = document.getElementById ("eventbody").getElementsByClassName ("event");
+	var option = $ ("#searchingoption").val ();
+	var events = $ (".event");
 	var matchedNum = 0;
-	if (option == "content") {
-		var string = document.getElementById ("searchingcontent").value;
-		var eventscontent = document.getElementById ("eventbody").getElementsByClassName ("content");
-		for (var i = 0 ; i < eventscontent.length ; i++) {
-			if (eventscontent[i].innerHTML.search (string) != -1) {
-				events[i].style.display = "table-row";
+	if (option == "title") {
+		var string = $ ("#searchinginput").val ();
+		var titles = $ (".title");
+		for (var i = 0 ; i < titles.length ; i++) {
+			if (titles[i].innerHTML.search (string) != -1) {
+				$ ("#event" + (i + 1) + "head").css ("display", "table-row");
+				$ ("#event" + (i + 1) + "content").css ("display", "none");
 				matchedNum++;
 			}
 			else {//Not Match
-				events[i].style.display = "none";
+				$ ("#event" + (i + 1) + "head").css ("display", "none");
+				$ ("#event" + (i + 1) + "content").css ("display", "none");
 			}
 		}
 	}
 	else if (option == "date") {
-		var month = document.getElementById ("searchingdatemonth").value;
-		var day = document.getElementById ("searchingdateday").value;
-		var eventsdate = document.getElementById ("eventbody").getElementsByClassName ("date");
+		var month = $ ("#searchingdatemonth").val ();
+		var day = $ ("#searchingdateday").val ();
+		var eventsdate = $ (".date");
 		var dateinfo = new Array ();
 		for (var i = 0 ; i < eventsdate.length ; i++) {
 			dateinfo = eventsdate[i].innerHTML.split (".");
 			if ((month == "" || dateinfo[1] == month) && (day == "" || dateinfo[2] == day)) {
-				events[i].style.display = "table-row";
+				$ ("#event" + (i + 1) + "head").css ("display", "table-row");
+				$ ("#event" + (i + 1) + "content").css ("display", "none");
 				matchedNum++;
 			}
 			else {//Not Match
-				events[i].style.display = "none";
+				$ ("#event" + (i + 1) + "head").css ("display", "none");
+				$ ("#event" + (i + 1) + "content").css ("display", "none");
+			}
+		}
+	}
+	else if (option == "content") {
+		var contents = $ (".content");
+		var string = $ ("#searchinginput").val ();
+		for (var i = 0 ; i < contents.length ; i++) {
+			if (contents[i].innerHTML.search (string) != -1) {
+				$ ("#event" + (i + 1) + "head").css ("display", "table-row");
+				$ ("#event" + (i + 1) + "content").css ("display", "none");
+				matchedNum++;
+			}
+			else {//Not Match
+				$ ("#event" + (i + 1) + "head").css ("display", "none");
+				$ ("#event" + (i + 1) + "content").css ("display", "none");
 			}
 		}
 	}
@@ -76,12 +94,24 @@ function changesearchingbar (obj) {
 }
 
 function togglecontent (eventid) {
-	var contentid, i;
+	var i;
 	for (i = 5 ; i < eventid.length ; i++) {
 		if (!(eventid[i] >= '0' && eventid[i] <= '9')) {
 			break;
 		}
 	}
-	contentid = eventid.substring (0, i) + "content";
-	$ ("#" + contentid).toggle (250);
+	eventid = eventid.substring (0, i);
+	$ ("#" + eventid + "content").toggle (250);
+}
+
+function changeyear (delta) {
+	var year = $ ("#year").text ();
+	year = parseInt (year) + delta;
+	$ ("#year").text (year.toString ());
+}
+
+function setyear () {
+	var today = new Date ();
+	var year = today.getFullYear ();
+	$ ("#year").text (year);
 }
