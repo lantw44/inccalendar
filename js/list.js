c0cce9dbfce8ebc228ec787c7cd4fcadd622f9e9
黃勁捷 <pizza159630@gmail.com>
2012-12-19 22:53:16 +0800

    list編輯畫面初步完成  目前確認按鈕還沒用

--- js/list.js
+++ js/list.js
@@ -1,6 +1,7 @@
 ﻿function pushevent () {//將活動放入行事曆中
 	var year = parseInt ($ ("#year").text ());
 	var dataclass = ["date", "week", "time", "title", "content"];
+	var dataid;
 	var Day = ["日", "一", "二", "三", "四", "五", "六"];
 	var eventid, dataid, datetime;
 	var calevent = new Array ();
@@ -13,15 +14,15 @@
 	for (var i = 0 ; i < calevent.length ; i++) {
 		eventid = "event" + (i + 1);
 		if ($ ("#" + eventid + "head").length == 0) {//這一行還沒有
-			$ ("#eventbody").append ("<tr id = \"" + eventid + "head\"></tr>");
+			$ ("#eventbody").append ("<tr id = '" + eventid + "head'></tr>");
 			$ ("#" + eventid + "head").addClass ("event");
 			for (var j = 0 ; j < 4 ; j++) {
 				dataid = eventid + dataclass[j];
-				$ ("#" + eventid + "head").append ("<td id = \"" + dataid + "\"></td>");
+				$ ("#" + eventid + "head").append ("<td id = '" + dataid + "'></td>");
 				$ ("#" + dataid).addClass (dataclass[j]);
 				$ ("#" + dataid).attr ({"onclick":"togglecontent (this.id);"});
 			}
-			$ ("#eventbody").append ("<tr id = \"" + eventid + "body\" class = \"event\"><td id = \"" + eventid + "content\"></td></tr>");
+			$ ("#eventbody").append ("<tr id = '" + eventid + "body' class = 'event'><td id = '" + eventid + "content'></td></tr>");
 			$ ("#" + eventid + "content").addClass ("content");
 			$ ("#" + eventid + "content").attr ({"colspan":"4"});
 		}
@@ -33,10 +34,18 @@
 			$ ("#" + eventid + dataclass[j]).text (calevent[i][dataclass[j]]);
 		}
 		$ ("#" + eventid + "content").css ("display", "none");
+		$ ("#" + eventid + "title").append ("<input type = 'button' value = '編輯' class = 'editbutton' onclick = 'editevent (\"" + eventid + "\")'></input>");
+		$ ("#" + eventid + "content").append ("<input type = 'button' value = '編輯' class = 'editbutton' onclick = 'editevent (\"" + eventid + "\")'></input>");
 	}
 	for (var i = calevent.length ; $ ("#event" + (i + 1) + "head").length > 0 ; i++) {//刪除多餘的空行
 		$ ("#event" + (i + 1) + "head").remove ();
 		$ ("#event" + (i + 1) + "body").remove ();
+	}
+	if (calevent.length == 0) {//沒有任何活動
+		$ ("#noeventmessage").css ("display", "block");
+	}
+	else {
+		$ ("#noeventmessage").css ("display", "none");
 	}
 }
 
@@ -100,6 +109,45 @@ function searchevent () {//搜尋符合的活動
 	}
 }
 
+function geteventid (dataid) {
+	var i;
+	for (i = 5 ; i < dataid.length ; i++) {
+		if (!(dataid[i] >= '0' && dataid[i] <= '9')) {
+			break;
+		}
+	}
+	return dataid.substring (0, i);
+}
+
+function editevent (eventid) {
+	var dataclass = ["date", "week", "time", "title", "content"];
+	var data = new Array ();
+	var datedata, timedata;
+	$ ("#" + eventid + "body").slideDown (250);
+	for (var i = 0 ; i < 5 ; i++) {
+		data.push ($ ("#" + eventid + dataclass[i]).text ());
+	}
+	//$ ("#" + eventid + "date").html ("<input type = 'date' id = 'input" + eventid + "date'></input>");
+	datedata = data[0].split (".");
+	$ ("#" + eventid + "date").html ("<input type = 'text' id = 'input" + eventid + "year' value = '" + datedata[0] + "' size = '2' /> 年");	//size = 2 因為size是算中文字
+	$ ("#" + eventid + "date").append ("<input type = 'text' id = 'input" + eventid + "month' value = '" + datedata[1] + "' size = '1' /> 月");
+	$ ("#" + eventid + "date").append ("<input type = 'text' id = 'input" + eventid + "date' value = '" + datedata[2] + "' size = '1' /> 日");
+	$ ("#" + eventid + "week").html ("<input type = 'tetxt' id = 'input" + eventid + "week' size = '3' value = '" + data[1] + "'/>");
+	timedata = data[2].split (":");
+	$ ("#" + eventid + "time").html ("<input type = 'tetxt' id = 'input" + eventid + "hour' value = '" + timedata[0] + "' size = '1' /> 時 <br />");
+	$ ("#" + eventid + "time").append ("<input type = 'tetxt' id = 'input" + eventid + "minute' value = '" + timedata[1] + "' size = '1' /> 分");
+	$ ("#" + eventid + "title").html ("<input type = 'tetxt' id = 'input" + eventid + "title' value = '" + data[3] + "'/>");
+	$ ("#" + eventid + "content").html ("<textarea id = 'input" + eventid + "content" + "'>" + data[4] + "</textarea>");
+	$ ("#input" + eventid + "content").attr ({"rows":"5", "cols":"100"});
+	$ ("#" + eventid + "content").append ("<input type = 'button' value = '確認' class = 'okbutton' />");
+	for (var i = 0 ; i < 4 ; i++) {
+		$ ("#" + eventid + dataclass[i]).removeAttr ("onclick");
+	}
+	if ($ ("#" + eventid + "content").css ("display") == "none") {
+		$ ("#" + eventid + "content").slideDown (250);
+	}
+}
+
 function changesearchingbar (obj) {
 	if (obj.value == "date") {
 		$ ("#searchingdate").css ("display", "inline");
@@ -111,15 +159,8 @@ function changesearchingbar (obj) {
 	}
 }
 
-function togglecontent (eventid) {
-	var i;
-	for (i = 5 ; i < eventid.length ; i++) {
-		if (!(eventid[i] >= '0' && eventid[i] <= '9')) {
-			break;
-		}
-	}
-	eventid = eventid.substring (0, i);
-	$ ("#" + eventid + "content").toggle (250);
+function togglecontent (dataid) {
+	$ ("#" + geteventid (dataid) + "content").toggle (250);
 }
 
 function changeyear (delta) {
@@ -132,4 +173,8 @@ function setyear () {
 	var today = new Date ();
 	var year = today.getFullYear ();
 	$ ("#year").text (year);
+}
+
+function setinitialcss () {
+	$ (".event:even").css ("cursor", "pointer");
 }
