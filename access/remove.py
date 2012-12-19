8fb65fa17ef54221c304ada7743bf79310a2e703
藍挺瑋 <lantw44@gmail.com>
2012-12-19 23:54:00 +0800

    剛剛少傳了一個檔案，現在補上 access/remove.py

---
+++ access/remove.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import webapp2
+
+from google.appengine.api import users
+from google.appengine.ext import db
+
+class RemoveEvent(webapp2.RequestHandler):
+	def get(self):
+		return
+	def post(self):
+		guserid = users.get_current_user()
+		if not guserid:
+			return
+		thiskey = db.Key(self.request.get('key'))
+		thisobj = db.get(thiskey)
+		thisobj.delete()
+
+app = webapp2.WSGIApplication([('/access/remove', RemoveEvent)])
