4c15c359836fd7f4dbb7a1680bfbb0c0582b0a04
黃勁捷 <pizza159630@gmail.com>
2012-12-20 11:03:18 +0800

    確認按鈕做好了  能夠顯示n行字串了

--- js/list.js
+++ js/list.js
@@ -1,16 +1,18 @@
-﻿function pushevent () {//將活動放入行事曆中
+﻿headdataclass = ["date", "week", "time", "title"];
+Day = ["日", "一", "二", "三", "四", "五", "六"];
+
+function escapestring (str) {//用出' ' && '\n'
+	str = str.replace (/\n/g, "<br/>");
+	str = str.replace (/ /g, "&nbsp;");
+	return str;
+}
+
+function pushevent () {//將活動放入行事曆中
 	var year = parseInt ($ ("#year").text ());
 	var dataclass = ["date", "week", "time", "title", "content"];
-	var dataid;
-	var Day = ["日", "一", "二", "三", "四", "五", "六"];
-	var eventid, dataid, datetime;
-	var calevent = new Array ();
-	for (var month = 1 ; month <= 12 ; month++) {
-		inccal_fetch (year, month);
-		for (var i = 0 ; i < caleventlist.length ; i++) {
-			calevent.push (caleventlist[i]);
-		}
-	}
+	var dataid, eventid, dataid, datetime, calevent;
+	inccal_fetch (year);
+	calevent = caleventlist;
 	for (var i = 0 ; i < calevent.length ; i++) {
 		eventid = "event" + (i + 1);
 		if ($ ("#" + eventid + "head").length == 0) {//這一行還沒有
@@ -24,15 +26,14 @@
 			}
 			$ ("#eventbody").append ("<tr id = '" + eventid + "body' class = 'event'><td id = '" + eventid + "content'></td></tr>");
 			$ ("#" + eventid + "content").addClass ("content");
-			$ ("#" + eventid + "content").attr ({"colspan":"4"});
+			$ ("#" + eventid + "content").attr ("colspan","4");
 		}
 		datetime = calevent[i]["datetime"];
 		$ ("#" + eventid + "date").text (datetime.getFullYear () + "." + (datetime.getMonth () + 1) + "." + datetime.getDate ());
 		$ ("#" + eventid + "week").text ("星期" + Day[datetime.getDay ()]);
 		$ ("#" + eventid + "time").text (datetime.getHours () + ":" + datetime.getMinutes ());
-		for (var j = 3 ; j < 5 ; j++) {
-			$ ("#" + eventid + dataclass[j]).text (calevent[i][dataclass[j]]);
-		}
+		$ ("#" + eventid + "title").text (calevent[i]["title"]);
+		$ ("#" + eventid + "content").html (escapestring (calevent[i]["content"]));
 		$ ("#" + eventid + "content").css ("display", "none");
 		$ ("#" + eventid + "title").append ("<input type = 'button' value = '編輯' class = 'editbutton' onclick = 'editevent (\"" + eventid + "\")'></input>");
 		$ ("#" + eventid + "content").append ("<input type = 'button' value = '編輯' class = 'editbutton' onclick = 'editevent (\"" + eventid + "\")'></input>");
@@ -119,29 +120,90 @@ function geteventid (dataid) {
 	return dataid.substring (0, i);
 }
 
-function editevent (eventid) {
-	var dataclass = ["date", "week", "time", "title", "content"];
-	var data = new Array ();
-	var datedata, timedata;
-	$ ("#" + eventid + "body").slideDown (250);
-	for (var i = 0 ; i < 5 ; i++) {
-		data.push ($ ("#" + eventid + dataclass[i]).text ());
+function changeweek (inputid) {
+	var eventid = geteventid (inputid.split ("input")[1]);
+	var date = new Date ();
+	date.setFullYear (parseInt ($ ("#input" + eventid + "year").val ()));
+	date.setMonth (parseInt ($ ("#input" + eventid + "month").val ()) - 1);
+	date.setDate (parseInt ($ ("#input" + eventid + "date").val ()));
+	$ ("#" + eventid + "week").text ("星期" + Day[date.getDay ()]);
+}
+
+function switchbacktonormalmode (eventid) {
+	var year, month, date, hour, minute, title, content;
+	year = $ ("#input" + eventid + "year").val ();
+	month = $ ("#input" + eventid + "month").val ();
+	date = $ ("#input" + eventid + "date").val ();
+	hour = $ ("#input" + eventid + "hour").val ();
+	minute = $ ("#input" + eventid + "minute").val ();
+	title = $ ("#input" + eventid + "title").val ();
+	content = $ ("#input" + eventid + "content").val ();
+	$ ("#" + eventid + "date").text (year + "." + month + "." + date);
+	$ ("#" + eventid + "time").text (hour + ":" + minute);
+	$ ("#" + eventid + "title").text (title);
+	$ ("#" + eventid + "content").html (escapestring (content));
+	$ ("#" + eventid + "title").append ("<input type = 'button' value = '編輯' class = 'editbutton' onclick = 'editevent (\"" + eventid + "\")'></input>");
+	$ ("#" + eventid + "content").append ("<input type = 'button' value = '編輯' class = 'editbutton' onclick = 'editevent (\"" + eventid + "\")'></input>");
+	for (var i = 0 ; i < headdataclass.length ; i++) {
+		$ ("#" + eventid + headdataclass[i]).attr ("onclick", "togglecontent (this.id)");
 	}
-	//$ ("#" + eventid + "date").html ("<input type = 'date' id = 'input" + eventid + "date'></input>");
-	datedata = data[0].split (".");
-	$ ("#" + eventid + "date").html ("<input type = 'text' id = 'input" + eventid + "year' value = '" + datedata[0] + "' size = '2' /> 年");	//size = 2 因為size是算中文字
-	$ ("#" + eventid + "date").append ("<input type = 'text' id = 'input" + eventid + "month' value = '" + datedata[1] + "' size = '1' /> 月");
-	$ ("#" + eventid + "date").append ("<input type = 'text' id = 'input" + eventid + "date' value = '" + datedata[2] + "' size = '1' /> 日");
-	$ ("#" + eventid + "week").html ("<input type = 'tetxt' id = 'input" + eventid + "week' size = '3' value = '" + data[1] + "'/>");
-	timedata = data[2].split (":");
-	$ ("#" + eventid + "time").html ("<input type = 'tetxt' id = 'input" + eventid + "hour' value = '" + timedata[0] + "' size = '1' /> 時 <br />");
-	$ ("#" + eventid + "time").append ("<input type = 'tetxt' id = 'input" + eventid + "minute' value = '" + timedata[1] + "' size = '1' /> 分");
-	$ ("#" + eventid + "title").html ("<input type = 'tetxt' id = 'input" + eventid + "title' value = '" + data[3] + "'/>");
-	$ ("#" + eventid + "content").html ("<textarea id = 'input" + eventid + "content" + "'>" + data[4] + "</textarea>");
+}
+
+function updateevent (eventid) {
+	var dataclass = ["title", "content", "icon", "remind", "datafrom"];
+	var calevent = new CalEvent ();
+	calevent.key = caleventlist[parseInt (eventid.split ("event")[1]) - 1].key;
+	for (var i = 0 ; i < dataclass.length ; i++) {
+		if ($ ("#input" + eventid + dataclass[i]).length > 0) {//存在這一個欄位
+			calevent[dataclass[i]] = $ ("#input" + eventid + dataclass[i]).val ();
+		}
+	}
+	calevent.datetime.setFullYear (parseInt ($ ("#input" + eventid + "year").val ()));
+	calevent.datetime.setMonth (parseInt ($ ("#input" + eventid + "month").val ()) - 1);
+	calevent.datetime.setDate (parseInt ($ ("#input" + eventid + "date").val ()));
+	calevent.datetime.setHours (parseInt ($ ("#input" + eventid + "hour").val ()));
+	calevent.datetime.setMinutes (parseInt ($ ("#input" + eventid + "month").val ()));
+	inccal_send (calevent);
+	switchbacktonormalmode (eventid);
+}
+
+function editevent (eventid) {
+	var dataclass = ["title", "content", "remind"];
+	var dateclass = ["year", "month", "date", "hour", "minute"];
+	var timedata = new Array ();
+	var data = new Array ();
+	var thisevent = caleventlist[parseInt (eventid.split ("event")[1]) - 1];
+	$ ("#" + eventid + "body").slideDown (250);
+	//$ ("#" + eventid + "date").html ("<input type = 'date' id = 'input" + eventid + "date'></input>");	//firefox不支援
+	$ ("#" + eventid + "date").html ("<input type = 'text' id = 'input" + eventid + "year' size = '2' maxlength = '4' onkeyup = 'changeweek (this.id);' /> 年");	//size = 2 因為size是算中文字
+	$ ("#" + eventid + "date").append ("<input type = 'text' id = 'input" + eventid + "month' size = '1' maxlength = '2' onkeyup = 'changeweek (this.id);'/> 月");
+	$ ("#" + eventid + "date").append ("<input type = 'text' id = 'input" + eventid + "date' size = '1' maxlength = '2' onkeyup = 'changeweek (this.id);'/> 日");
+	$ ("#" + eventid + "time").html ("<input type = 'tetxt' id = 'input" + eventid + "hour' size = '1' maxlength = '2' /> 時 <br />");
+	$ ("#" + eventid + "time").append ("<input type = 'tetxt' id = 'input" + eventid + "minute' size = '1' maxlength = '2' /> 分");
+	$ ("#" + eventid + "title").html ("<input type = 'tetxt' id = 'input" + eventid + "title' size = '40' />");
+	$ ("#" + eventid + "content").html ("<div> 活動內容： </div><textarea id = 'input" + eventid + "content" + "'></textarea><br />");
 	$ ("#input" + eventid + "content").attr ({"rows":"5", "cols":"100"});
-	$ ("#" + eventid + "content").append ("<input type = 'button' value = '確認' class = 'okbutton' />");
-	for (var i = 0 ; i < 4 ; i++) {
-		$ ("#" + eventid + dataclass[i]).removeAttr ("onclick");
+	$ ("#" + eventid + "content").append ("提醒：<input type = 'tetxt' id = 'input" + eventid + "remind' size = '1' maxlength = '4' /> 分鐘前&nbsp;&nbsp;&nbsp;&nbsp;");
+	$ ("#" + eventid + "content").append ("資料來源： #include <行事曆.h>&nbsp;&nbsp;&nbsp;&nbsp;");
+	$ ("#" + eventid + "content").append ("<input type = 'button' value = '確認' class = 'okbutton' onclick = 'updateevent (\"" + eventid + "\");' />");
+	for (var i = 0 ; i < dataclass.length ; i++) {
+		if (dataclass[i] == "content") {
+			$ ("#input" + eventid + "content").text (thisevent[dataclass[i]]);//textarea
+		}
+		else {
+			$ ("#input" + eventid + dataclass[i]).val (thisevent[dataclass[i]]);//input type=text
+		}
+	}
+	timedata.push (thisevent.datetime.getFullYear ());
+	timedata.push (thisevent.datetime.getMonth () + 1);
+	timedata.push (thisevent.datetime.getDate ());
+	timedata.push (thisevent.datetime.getHours ());
+	timedata.push (thisevent.datetime.getMinutes ());
+	for (var i = 0 ; i < timedata.length ; i++) {
+		$ ("#input" + eventid + dateclass[i]).val (timedata[i]);
+	}
+	for (var i = 0 ; i < headdataclass.length ; i++) {
+		$ ("#" + eventid + headdataclass[i]).removeAttr ("onclick");
 	}
 	if ($ ("#" + eventid + "content").css ("display") == "none") {
 		$ ("#" + eventid + "content").slideDown (250);
